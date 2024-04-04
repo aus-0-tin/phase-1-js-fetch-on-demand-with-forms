f6dc5313ddb4d75e6a279f9f5d0ec19c9ccb28c6
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-04-04 14:14:23 +0300

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+    //Grab the form Element
+    const inputForm = document.querySelector("form");
+
+    //Adding event listener
+    inputForm.addEventListener("submit", (event) => { 
+
+        //Stops the page from refreshing
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+
+         title.innerText = data.title;
+         summary.innerText = data.summary;
+     });
+   });  
+}; 
 
 document.addEventListener('DOMContentLoaded', init);
